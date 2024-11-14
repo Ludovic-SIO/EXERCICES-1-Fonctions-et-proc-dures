a27df7c144c72bf9ab22e8dffc558ba7f40f307b
lchamprobert <lchamprobert@N112-04>
2024-11-14 16:34:16 +0100

    exo 3

--- EXERCICES 1 Fonctions et procédures/Program.cs
+++ EXERCICES 1 Fonctions et procédures/Program.cs
@@ -9,28 +9,31 @@ namespace EXERCICES_1_Fonctions_et_procédures
     internal class Program
     {
 
-        static int difference(int a, int b)
+        static string adresse (string nom, string prenom, string domaine)
         {
-            int somme=a+b;
-            int produit = a * b;
-            return produit - somme;
+            string a=".";
+            string b = "@";
+        return prenom +a   +nom +b +domaine;
             
         }
         static void Main(string[] args)
         {
            
-            int a;
-            int b;
+            string nom;
+            string prenom;
+            string domaine;
             
-                Console.WriteLine("Saisie un entier");
-                a = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("la valeur est {0}", a);
-                Console.WriteLine("Saisie un autre entier");
-                b = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("la valeur est {0}", b);
+                Console.WriteLine("Saisie ton nom");
+                nom=Console.ReadLine();
+                Console.WriteLine("le nom est {0}", nom);
+                Console.WriteLine("Saisie ton prénom");
+                prenom=Console.ReadLine();
+                Console.WriteLine("le prenom est {0}", prenom);
+                Console.WriteLine("Saisie ton domaine");
+                domaine=Console.ReadLine();
+                Console.WriteLine("la valeur est {0}", domaine);
 
-
-                Console.WriteLine("La différence entre le produit et la somme est: {0}",difference (a,b));
+                Console.WriteLine("L'adresse mail est: {0}",adresse(nom,prenom,domaine));
                 Console.ReadKey();
 
         }
