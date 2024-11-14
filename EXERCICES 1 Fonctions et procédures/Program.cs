d4691194bfa9ffa8925db5b025b208f3588002a5
lchamprobert <lchamprobert@N112-04>
2024-11-14 15:54:51 +0100

    Ajoutez des fichiers projet.

---
+++ EXERCICES 1 Fonctions et procédures/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EXERCICES_1_Fonctions_et_procédures
+{
+    internal class Program
+    {
+
+        static int absolue(int a)
+        {
+            if (a >= 0) return a;
+            else return -a;
+        }
+        static void Main(string[] args)
+        {
+           
+                int a;
+
+                Console.WriteLine("Saisie le valeur entière d'un nombre");
+                a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("la valeur est {0}", a);   
+                
+                Console.WriteLine("La valeur absolu est: {0}",absolue (a));
+                Console.ReadKey();
+
+        }
+    }
+}
---
+++ EXERCICES 1 Fonctions et procédures/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("EXERCICES 1 Fonctions et procédures")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("EXERCICES 1 Fonctions et procédures")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("bb9d46a7-73f2-4d85-930b-c76b8f264b4b")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire
+//      Numéro de build
+//      Révision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
